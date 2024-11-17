d8b2d1e78edc5fa254ab25001d615a77d07674a5
ASAK2006 <group9practical@gmail.com>
2024-11-17 17:19:57 +0530

    updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -19,21 +19,26 @@ export const fetchDataFromApi = async (url) => {
     }
 };
 
-
-
 const getAuthorizationHeader = () => {
-    const apiKey = "e0c25f7fcfdaf02a505ddc26123cb3fd5fca07733cd836dcbd4a858e3c3f265c7694905cb339f0c599750bc75a1b3e0c6ff6dabbe9ab7f6ee465021040d41cd191f0f45785fdba3b793500cc705586eb3e60f38a8ed1300d5ffafcfb3d02dff78c7ec097ae9741d8583e46321d5f480e24b7cb08fda3da8bba18a59797b4b3bc";
+    const apiKey = process.env.REACT_APP_STRIPE_APP_KEY;
     if (!apiKey) {
-      console.error("Stripe API key not set in environment variables");
+        console.error("Stripe API key not set in environment variables");
     }
     return {
-      Authorization: `Bearer ${apiKey}`,
+        Authorization: `Bearer ${apiKey}`,
     };
-  };
-  
-  export const makePaymentRequest = axios.create({
-    baseURL: "http://localhost:1337",  
-    headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_STRIPE_APP_KEY}`,
+};
+
+export const makePaymentRequest = async (url, data) => {
+    try {
+        const response = await axios.post(
+            process.env.REACT_APP_DEV_URL + url,
+            data,
+            { headers: getAuthorizationHeader() }
+        );
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return err;
     }
-  });
+};
